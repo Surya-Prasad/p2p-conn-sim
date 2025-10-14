0b9ec186e6b345d3e84739ec13ef078cd0dfa57f
Surya Prasad Senthilkumaran <suryapra@Suryas-MacBook-Air.local>
2025-10-13 23:15:15 -0400

    Server init_logic

---
+++ p2p-server/server-socket.py
@@ -0,0 +1,19 @@
+import socket
+import threading
+import json, hashlib
+import argparse
+import os
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server_port = 3939
+sock.bind(('', server_port))
+print(f"Server bound to {server_port}")
+
+sock.listen()
+
+while True:
+    conn, addr = sock.accept()
+    # threading.Thread(target=handle_peer, args=(conn,)).start() // Peer-Handling logic here. 
+    print(f"Connected to {conn} and {addr}")
+
